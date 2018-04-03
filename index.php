4b68f0dfb31f871961aac5360f1633ab23ad5bcc
alliza <bistaaliza@gmail.com>
2018-04-03 06:46:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,169 +1,96 @@
 
 <html>
 <head>
-<!--<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> -->
 <title>Login with Google and Facebook</title>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rl="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <style type="text/css">
-h1{font-family:Arial, Helvetica, sans-serif;color:#999999;}
+h1{ font-family:Arial, Helvetica, sans-serif;
+    color:63b0e6;
+    }
+h8{
+    font-family:Arial, Helvetica, sans-serif;
+    color:63b0e6;
+    font-size: 1.5em;
+    padding: 30%;
+}
+p{
+   font-family:Arial, Helvetica,sans-serif;
+    color:63b0e6; 
+    font-size: 1.17em;
+}
+body{
+    background:#071a44;
+}
+.navbar{
+        background-color: #071a44;
+        color: #4d0066;
+        padding: 5% 0;
+        font-size: 1.2em;
+        height:70px;
+}
+.container{
+    padding-left: 25%;
+}
+.container1{
+    height:25%;
+    width: 70%;
+    background-color: #a068b1;
+    margin-bottom: 10px;
+    padding: 5px 5px 5px 5px;
+}
+.container2{
+    height:15%;
+    width: 70%;
+    background-color: #a068b1;
+    padding: 5px 5px 5px 5px;
+    margin-bottom: 5px;
+}
+.content1{
+    height:100%;
+    width: 100%;
+    background: #230741;
+    padding: 5% 5% 5% 5%;
+}
+.content2{
+    height:100%;
+    width: 100%;
+    background: #230741;
+    padding:0% 10% 10% 10%;
+}
 </style>
 </head>
 <body>
-  <h1 align="Center"> Schedulr </h1>
-  <div align="Center">
-<?php
-//Include Google, Facebook config file && User class
-include_once 'gpConfig.php';
-include_once 'User.php';
-
-if(isset($_GET['code'])){
-  $gClient->authenticate($_GET['code']);
-  $_SESSION['token'] = $gClient->getAccessToken();
-  header('Location: ' . filter_var($redirectURL, FILTER_SANITIZE_URL));
-}
-
-if (isset($_SESSION['token'])) {
-  $gClient->setAccessToken($_SESSION['token']);
-}
-
-if ($gClient->getAccessToken()) {
-  //Get user profile data from google
-  $gpUserProfile = $google_oauthV2->userinfo->get();
-  
-  //Initialize User class
-  $user = new User();
-  
-  //Insert or update user data to the database
-    $gpUserData = array(
-        'oauth_provider'=> 'google',
-        'oauth_uid'     => $gpUserProfile['id'],
-        'first_name'    => $gpUserProfile['given_name'],
-        'last_name'     => $gpUserProfile['family_name'],
-        'email'         => $gpUserProfile['email'],
-        'locale'        => $gpUserProfile['locale'],
-        'picture'       => $gpUserProfile['picture'],
-        'link'          => $gpUserProfile['link']
-    );
-    $userData = $user->checkUser($gpUserData);
-  
-  //Storing user data into session
-  $_SESSION['userData'] = $userData;
-  
-  //Render facebook profile data
-    if(!empty($userData)){
-      $output = 'Hello';
-        $output = '<h1>Google+ Profile Details </h1>';
-        $output .= '<img src="'.$userData['picture'].'" width="300" height="220">';
-        $output .= '<br/>Google ID : ' . $userData['oauth_uid'];
-        $output .= '<br/>Name : ' . $userData['first_name'].' '.$userData['last_name'];
-        $output .= '<br/>Email : ' . $userData['email'];
-        $output .= '<br/>Gender : ' . $userData['gender'];
-        $output .= '<br/>Locale : ' . $userData['locale'];
-        $output .= '<br/>Logged in with : Google';
-        $output .= '<br/><a href="'.$userData['link'].'" target="_blank">Click to Visit Google+ Page</a>';
-        $output .= '<br/>Logout from <a href="logout.php">Google</a>'; 
-    }else{
-        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
-    }
-} else {
-  $authUrl = $gClient->createAuthUrl();
-  $output = '<a href="'.filter_var($authUrl, FILTER_SANITIZE_URL).'"><img src="images/googlein.png" alt=""/></a>';
-}
-?>
-  <?php echo $output; ?></div>
-<div align="Center">
-<?php
-// Include FB config file && User class
-require_once 'fbconfig.php';
-require_once 'User.php';
-
-if(isset($accessToken)){
-    if(isset($_SESSION['facebook_access_token'])){
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    }else{
-        // Put short-lived access token in session
-        $_SESSION['facebook_access_token'] = (string) $accessToken;
-        
-          // OAuth 2.0 client handler helps to manage access tokens
-        $oAuth2Client = $fb->getOAuth2Client();
-        
-        // Exchanges a short-lived access token for a long-lived one
-        $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-        $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-        
-        // Set default access token to be used in script
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    }
-    
-    // Redirect the user back to the same page if url has "code" parameter in query string
-    if(isset($_GET['code'])){
-        header('Location: ./');
-    }
-    
-    // Getting user facebook profile info
-    try {
-        $profileRequest = $fb->get('/me?fields=name,first_name,last_name,email,link,gender,locale,cover,picture');
-        $fbUserProfile = $profileRequest->getGraphNode()->asArray();
-    } catch(FacebookResponseException $e) {
-        echo 'Graph returned an error: ' . $e->getMessage();
-        session_destroy();
-        // Redirect user back to app login page
-        header("Location: ./");
-        exit;
-         } catch(FacebookSDKException $e) {
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-        exit;
-    }
-    
-    // Initialize User class
-    $user = new User();
-    
-    // Insert or update user data to the database
-    $fbUserData = array(
-        'oauth_provider'=> 'facebook',
-        'oauth_uid'     => $fbUserProfile['id'],
-        'first_name'    => $fbUserProfile['first_name'],
-        'last_name'     => $fbUserProfile['last_name'],
-        'email'         => $fbUserProfile['email'],
-        'gender'        => $fbUserProfile['gender'],
-        'locale'        => $fbUserProfile['locale'],
-        'cover'         => $fbUserProfile['cover']['source'],
-        'picture'       => $fbUserProfile['picture']['url'],
-        'link'          => $fbUserProfile['link']
-    );
-    $userData = $user->checkUser($fbUserData);
-    // Put user data into session
-    $_SESSION['userData'] = $userData;
-    
-    // Get logout url
-    $logoutURL = $helper->getLogoutUrl($accessToken, $redirectURL.'logout.php');
-    
-    // Render facebook profile data
-    if(!empty($userData)){
-        $output  = '<h2 style="color:#999999;">Facebook Profile Details</h2>';
-        $output .= '<div style="position: relative;">';
-        //$output .= '<img src="'.$userData['cover'].'" />';
-        $output .= '<img src="'.$userData['picture'].'" width="300" height="220">';
-        $output .= '</div>';
-        $output .= '<br/>Facebook ID : '.$userData['oauth_uid'];
-        $output .= '<br/>Name : '.$userData['first_name'].' '.$userData['last_name'];
-        $output .= '<br/>Email : '.$userData['email'];
-        $output .= '<br/>Gender : '.$userData['gender'];
-        $output .= '<br/>Locale : '.$userData['locale'];
-         $output .= '<br/>Logged in with : Facebook';
-        $output .= '<br/>Profile Link : <a href="'.$userData['link'].'" target="_blank">Click to visit Facebook page</a>';
-          $output .= '<br/>Logout from <a href="logout1.php">Facebook</a>'; 
-    }else{
-        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
-    }
-    
-}else{
-    // Get login url
-    $loginURL = $helper->getLoginUrl($redirectURL, $fbPermissions);
-    
-    // Render facebook login button
-    $output = '<a href="'.htmlspecialchars($loginURL).'"><img src="images/fblogin.png"></a>';
-}
-?>
-<?php echo $output; ?></div>
+  <div class = "navbar">
+      <h1 align="Center"> Welcome To Schedulr </h1>
+  </div>
+  <div class="container">
+      <div class="container1">
+        <div class="content1">
+            <p>Schedulr is a web application designed by students for students. Its ourpose is to make scheduling meetings and events easier by selecting the best time for everyone to meet.</p>
+        </div>
+      </div>
+      <div class="container2">
+          <div class="content2">
+           <h8>Schedulr</h8>
+               <p> Please choose a sign in option: </p>
+          </div>
+      </div>
+  </div>
+  <div class="container-fluid">
+    <div class="col-xs-12 col-md-8">
+            <?php
+            include_once 'google.php';
+            ?>
+             <?php echo $output; ?></div> 
+    <div class="col-xs-6 col-md-4">
+        <?php
+        include_once 'facebook.php';
+        ?>
+         <?php echo $output; ?></div>
 </body>
 </html>
